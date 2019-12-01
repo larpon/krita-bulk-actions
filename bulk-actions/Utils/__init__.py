49ef0488162a2e29c7d0558f6b709fc67affb4ed
lmp <dev.larpon@gmail.com>
2019-12-01 17:52:02 +0100

    Add remap and clamp to Utils

--- bulk-actions/Utils/__init__.py
+++ bulk-actions/Utils/__init__.py
@@ -5,3 +5,12 @@ def flip(f):
 
 def kickstart(it):
     deque(it, maxlen=0)
+
+def remap(oldValue, oldMin, oldMax, newMin, newMax):
+    # Linear conversion
+    if oldMin == newMin and oldMax == newMax:
+        return oldValue;
+    return (((oldValue - oldMin) * (newMax - newMin)) / (oldMax - oldMin)) + newMin;
+
+def clamp(value, clamp_min, clamp_max):
+    return min(clamp_max, max(clamp_min, value))
